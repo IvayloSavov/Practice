5f2d759404917330e2d34654e145f1e2de3b9283
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-11-14 14:10:28 +0200

    Update no_vowels.py

---
+++ Lists Advanced - Lab/no_vowels.py
@@ -0,0 +1,7 @@
+# text = input()
+#
+# vowels = ['a', 'o', 'u', 'e', 'i']
+# print("".join([letter for letter in text if letter.lower() not in vowels]))
+#
+print("".join([letter for letter in input() if letter.lower() not in ['a', 'o', 'u', 'e', 'i']]))
+
