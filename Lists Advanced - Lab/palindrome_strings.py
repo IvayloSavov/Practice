f3c32f723276122924f8f6713665fad982561669
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-11-14 15:12:15 +0200

    Update palindrome_strings.py

--- Lists Advanced - Lab/palindrome_strings.py
+++ Lists Advanced - Lab/palindrome_strings.py
@@ -1,6 +1,11 @@
-words = input().split(" ")
-palindrome = input()
-palindromes = [word for word in words if word == word[::-1]]
+# words = input().split(" ")
+# palindrome = input()
+# palindromes = [word for word in words if word == word[::-1]]
+#
+# print(palindromes)
+# print(f"Found palindrome {palindromes.count(palindrome)} times")
+
+palindromes = [word for word in input().split(" ") if word == word[::-1]]
 
 print(palindromes)
-print(f"Found palindrome {palindromes.count(palindrome)} times")
+print(f"Found palindrome {palindromes.count(input())} times")
