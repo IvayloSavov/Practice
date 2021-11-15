b554e4b65b934fd776edadc2d1ba4e54f7e62ef6
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-11-15 10:57:45 +0200

    Update Group_of_10's.py

--- Lists Advanced - Exercise/Group_of_10's.py
+++ Lists Advanced - Exercise/Group_of_10's.py
@@ -1,1 +1,10 @@
-numbers = map(int, input().split(", "))
+numbers = list(map(int, input().split(", ")))
+
+current_group = 10
+
+while len(numbers) > 0:
+    numbers_in_current_group = [n for n in numbers if n <= current_group]
+    numbers = [n for n in numbers if n > current_group]
+
+    print(f"Group of {current_group}'s: {numbers_in_current_group}")
+    current_group += 10
