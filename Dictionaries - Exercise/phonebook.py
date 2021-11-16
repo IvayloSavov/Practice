a97c0fb38e13b10d389d91c47e4abdb8654f3531
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-11-16 16:14:24 +0200

    Update phonebook.py

--- Dictionaries - Exercise/phonebook.py
+++ Dictionaries - Exercise/phonebook.py
@@ -1,7 +1,17 @@
-command = input()
 phonebook = {}
 
 while True:
-    if command.isdigit():
+    command = input()
+
+    if len(command) == 1:
         break
-    
+    name, number = command.split("-")
+    phonebook[name] = number
+
+
+for _ in range(int(command)):
+    contact_name = input()
+    if contact_name in phonebook:
+        print(f"{contact_name} -> {phonebook[contact_name]}")
+    else:
+        print(f"Contact {contact_name} does not exist.")
