1b48fc2d017564cf832918ef15a010f6adfc37ad
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-11-20 16:31:25 +0200

    Create courses.py

---
+++ Dictionaries - Exercise/courses.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+courses = defaultdict(list)
+line = input()
+
+while line != "end":
+    course_name, student_name = line.split(" : ")
+    courses[course_name].append([student_name])
+
+    line = input()
+
+for course, students in sorted(courses.items(), key=lambda c: -len(c[1])):
+    print(f"{course}: {len(students)}")
+    for student in sorted(students):
+        print(f"-- {student[0]}")
