9eda740dc6624fbc6a132058fccaa1a50ebf4c35
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-11-20 16:34:51 +0200

    Update courses.py

--- Dictionaries - Exercise/courses.py
+++ Dictionaries - Exercise/courses.py
@@ -5,11 +5,11 @@ line = input()
 
 while line != "end":
     course_name, student_name = line.split(" : ")
-    courses[course_name].append([student_name])
+    courses[course_name].append(student_name)
 
     line = input()
 
 for course, students in sorted(courses.items(), key=lambda c: -len(c[1])):
     print(f"{course}: {len(students)}")
     for student in sorted(students):
-        print(f"-- {student[0]}")
+        print(f"-- {student}")
