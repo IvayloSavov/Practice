c5315ebfb88fc9a72271dac77edba62ebc714484
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-11-23 15:29:09 +0200

    Solved Need for speed problem

---
+++ Inheritance - Exercise/need_for_speed/project/car.py
@@ -0,0 +1,9 @@
+from project.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    DEFAULT_FUEL_CONSUMPTION: float = 3.00
+
+    def __init__(self, fuel, horse_power):
+        super().__init__(fuel, horse_power)
+
