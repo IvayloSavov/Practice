8dd0752fb32a50ddbdf8c04a3ccf2482be7557e2
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-11-25 10:56:41 +0200

    Update trainer.py

--- Static and Class Methods - Exercise/gym/project/trainer.py
+++ Static and Class Methods - Exercise/gym/project/trainer.py
@@ -1,1 +1,14 @@
-from
+class Trainer:
+    id = 1
+
+    def __init__(self, name: str):
+        self.name = name
+        self.id = self.__class__.id
+        self.__class__.id += 1
+
+    def __repr__(self):
+        return f"Trainer <{self.id}> {self.name}"
+
+    @staticmethod
+    def get_next_id():
+        return Trainer.id
