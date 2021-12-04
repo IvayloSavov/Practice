f533df0bc018443e3eabbba1ce867aa659c101a3
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-12-04 12:31:39 +0200

    Update vehicle.py

--- Polymorphism and Abstraction/vehicle.py
+++ Polymorphism and Abstraction/vehicle.py
@@ -8,19 +8,33 @@ class Vehicle(ABC):
 
     @abstractmethod
     def drive(self, distance):
-        fuel_needed = distance * (self.fuel_consumption + (self.__class__.air_con_per_kilometer * distance)
-        
+        pass
 
     @abstractmethod
     def refuel(self, fuel):
-        self.fuel_quantity += fuel
+        pass
 
 
 class Car(Vehicle):
     air_con_per_kilometer = 0.9
-    pass
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+    def drive(self, distance):
+        fuel_needed = distance * (self.fuel_consumption + self.air_con_per_kilometer)
+        if fuel_needed <= self.fuel_quantity:
+            self.fuel_quantity -= fuel_needed
 
 
 class Truck(Vehicle):
     air_con_per_kilometer = 1.6
-    pass
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel * 0.95
+
+    def drive(self, distance):
+        fuel_needed = distance * (self.fuel_consumption + self.air_con_per_kilometer)
+        if fuel_needed <= self.fuel_quantity:
+            self.fuel_quantity -= fuel_needed
+
