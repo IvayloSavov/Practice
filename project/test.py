28fcd78ec34d325262a20f246c5ead2cf97da0fb
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-12-07 18:59:15 +0200

    Create test.py

---
+++ project/test.py
@@ -0,0 +1,9 @@
+from project.Tomcat import Tomcat
+from project.dog import Dog
+
+dog = Dog("Rocky", 3, "Male")
+print(dog.make_sound())
+print(dog)
+tomcat = Tomcat("Tom", 6)
+print(tomcat.make_sound())
+print(tomcat)
