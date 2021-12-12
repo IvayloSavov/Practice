098187c47ed325f1a8b26a0630bd3d727bb3c23d
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-12-12 11:01:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print(tuple([1, 2]))
+def jumpingOnClouds(c):
+    clouds = c
+    index = 0
+    moves = 0
+    while index <= len(clouds) - 1:
+        if index + 2 in range(len(clouds)) and clouds[index + 2] != "1":
+            index += 2
+            moves += 1
+        elif index + 1 in range(len(clouds)) and clouds[index + 1] != "1":
+            index += 1
+            moves += 1
+        else:
+            break
+
+    return moves
+
+
+jumpingOnClouds([0, 0, 1, 0, 0, 1, 0])
