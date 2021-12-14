4598cfe9cf45f69b19538e8c04765b7d6fd987aa
Ivaylo Savov <70910475+IvayloSavov@users.noreply.github.com>
2021-12-14 19:28:35 +0200

    Update test_2.py

--- test_2.py
+++ test_2.py
@@ -1,8 +1,59 @@
-def missingNumbers(arr, brr):
-    set_1 = set(brr)
-    set_2 = set(arr)
+#
+# import math
+# import os
+# import random
+# import re
+# import sys
+#
+#
+# #
+# # Complete the 'closestNumbers' function below.
+# #
+# # The function accepts INTEGER_ARRAY numbers as parameter.
+# #
+#
+# def closestNumbers(numbers):
+#     minimal_absolute_difference = sys.maxsize
+#     for i in range(len(numbers)):
+#         for j in range(i + 1, len(numbers)):
+#             n_1 = numbers[i]
+#             n_2 = numbers[j]
+#             cur_difference = abs(n_1 - n_2)
+#             if cur_difference < minimal_absolute_difference and cur_difference != 0:
+#                 minimal_absolute_difference = cur_difference
+#
+#     pairs = []
+#
+#     for i in range(len(numbers)):
+#         for j in range(i + 1, len(numbers)):
+#             n_1 = numbers[i]
+#             n_2 = numbers[j]
+#             cur_difference = abs(abs(n_1) - abs(n_2))
+#             if cur_difference == minimal_absolute_difference:
+#                 pairs.append([n_1, n_2])
+#
+#     sorted_pairs = sorted(pairs, key=lambda p: (p[0], p[1]))
+#     for p in sorted_pairs:
+#         p.sort()
+#         print(" ".join(map(str, p)))
+#
+#
+# closestNumbers([4, 4, -2, -1, 3])
 
-    diff = set_1.difference(set_2)
-    print(list(diff))
+.
+What is the output of the below code?
 
-missingNumbers([1, 4], [1, 2, 3, 4])
+class X:
+    def __init__(self):
+        self.__num1 = 5
+        self.num2 = 2
+
+    def display_values(self):
+        print(self.__num1, self.num2)
+class Y(X):
+    def __init__(self):
+        super().__init__()
+        self.__num1 = 1
+        self.num2 = 6
+obj = Y()
+obj.display_values()
